b456111b03d1befe8eb89dd8adc7985ac6e0e343
Gilbert <gssenyonjo@gmail.com>
2022-05-01 08:19:18 +0300

    looked at forgetting compositions

--- app/src/main/java/net/passioncloud/hellostate/WaterCounter.kt
+++ app/src/main/java/net/passioncloud/hellostate/WaterCounter.kt
@@ -1,6 +1,7 @@
 package net.passioncloud.hellostate
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -8,8 +9,18 @@ import androidx.compose.runtime.*
 
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.passioncloud.hellostate.ui.theme.HelloStateTheme
 
+
+@Preview
+@Composable
+fun WellnessScreenPreview() {
+    HelloStateTheme {
+        WellnessScreen()
+    }
+}
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
@@ -17,22 +28,50 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier =Modifier) {
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column {
-        var count by remember { mutableStateOf(30) }
+        var count by remember { mutableStateOf(0) }
 
-        Text(
-            text="You have had $count glasses.",
-            modifier=modifier.padding(16.dp)
-        )
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) } // will be forgotten when count == 0
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you done a walk",
+                    onClose = { showTask = false })
+            }
+            Text(
+                text = "You have had $count glasses.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
 
-        Button(
-            onClick = {
-                count++;
-            },
-            modifier=Modifier.padding(top=8.dp)
+        Row(
+            Modifier.padding(top=8.dp)
         ) {
-            Text("Add one to $count")
+            Button(
+                onClick = {
+                    count++;
+                },
+                modifier = Modifier.padding(top = 8.dp),
+                enabled = count < 10
+            ) {
+                Text("Add one")
+            }
+
+            Button(
+                onClick = {
+                    count--
+                },
+                modifier = Modifier.padding(top = 8.dp),
+                enabled = count > 0
+            ) {
+                Text("Minus one")
+            }
+            Button(
+                onClick = { count = 0 }
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
